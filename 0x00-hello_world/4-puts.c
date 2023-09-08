9eda4a68734f6a4c5f9ef90c4cf4bacc672385d7
Akunnacode <akunnabell@gmail.com>
2023-09-08 08:05:59 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that prits a line with puts fnction
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
